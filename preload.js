e516818f3940257835d60dc65f399a7a9539a954
Colin Au Yeung <daisuke.moto403@gmail.com>
2021-08-20 10:11:44 -0600

    update

--- preload.js
+++ preload.js
@@ -299,8 +299,8 @@ function startRecording() {
                 // width: { min: 1024, ideal: 1280, max: 1920 },
                 // height: { min: 576, ideal: 720, max: 1080 },
                 // chromeMediaSourceId: 'e0dba54a7062f30afbe7a3f906e2a69b4eff636357031793248e1547197dd3b7',
-                // chromeMediaSourceId: 'a9a5941dca194c65010c55af6890de13d367371d675be7d62a6a03af7f555e42',
-                chromeMediaSourceId: '69a54c6d837ebced4288488713136ac5db3badbde5d838ff51779f5ec47cd2c1',
+                chromeMediaSourceId: 'a9a5941dca194c65010c55af6890de13d367371d675be7d62a6a03af7f555e42',
+                // chromeMediaSourceId: '69a54c6d837ebced4288488713136ac5db3badbde5d838ff51779f5ec47cd2c1',
             }
             // width: { ideal: 4096 },
             // height: { ideal: 2160 }
